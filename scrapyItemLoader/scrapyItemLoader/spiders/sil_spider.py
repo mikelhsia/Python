36831c05f5b05bf1552e52d877ac25ecbc7fec19
mikelhsia <mikelhsia@hotmail.com>
2017-07-31 16:21:59 +0800

    Remove redundant comments and following PEP8 format by folding the string that is too long

--- scrapyItemLoader/scrapyItemLoader/spiders/sil_spider.py
+++ scrapyItemLoader/scrapyItemLoader/spiders/sil_spider.py
@@ -10,14 +10,14 @@ class SilSpiderSpider(scrapy.Spider):
 	name = 'sil_spider'
 	allowed_domains = ["manhua.fzdm.com/"]
 
-	manga      = int(raw_input("食戟之靈 - 58\t七原罪 - 56\t辛巴达 - 119\n猎人 - 10\t东京RE - 117\n进击 - 39\t英雄学院 - 131\nInput Manga code: "))
+	manga      = int(raw_input("食戟之靈 - 58\t七原罪 - 56\t辛巴达 - 119\n"
+	                           "猎人 - 10\t东京RE - 117\n"
+	                           "进击 - 39\t英雄学院 - 131"
+	                           "\nInput Manga code: "))
 	targetChap = int(raw_input("Input start downloading chapter: "))
 	numChap    = int(raw_input("How many chapters you want to download: "))
 
 	start_urls = ["http://manhua.fzdm.com/%d/%d/" % (manga, chap) for chap in xrange(targetChap, targetChap+numChap)]
-	# for x in range(numChap):
-	# 	start_urls.append("http://manhua.fzdm.com/%d/%d/" % (manga, targetChap+x))
-	# 	print "http://manhua.fzdm.com/%d/%d/" % (manga, targetChap+x)
 
 	def parse(self, response):
 		# meta字段是传递值的方法。在调试时返回的response中会出现meta的内容，它是一个字典，
@@ -25,7 +25,9 @@ class SilSpiderSpider(scrapy.Spider):
 		# (也可以使用get的方法，附默认值防止出现异常）
 		# 加了dont_filter=True的参数就完全可以用了！Why!?
 		# self.log("1. [URL Parsing and Downloading Image]: %s" % response.url)
-		yield Request(url=response.url, callback=self.parse_detail, dont_filter=True)
+		yield Request(url=response.url,
+		              callback=self.parse_detail,
+		              dont_filter=True)
 
 		## We want to inspect one specific response
 		# 您可以点击Ctrl-D(Windows下Ctrl-Z)来退出终端
@@ -40,12 +42,13 @@ class SilSpiderSpider(scrapy.Spider):
 
 			if next_link:
 				# self.log("3. [URL Parsing - Goes to next link]: %s" % next_link)
-				yield Request(url=urlparse.urljoin(response.url, next_link), callback=self.parse, dont_filter=True)
+				yield Request(url=urlparse.urljoin(response.url, next_link),
+				              callback=self.parse,
+				              dont_filter=True)
 				# 注：可以修改settings.py 中的配置文件，以此来指定“递归”的层数，如： DEPTH_LIMIT = 1
 		except:
 			# self.log("4. [URL Parsing]: Nothing to be poped")
 			pass
-
 
 	def parse_detail(self, response):
 		item = ScrapyitemloaderItem()
