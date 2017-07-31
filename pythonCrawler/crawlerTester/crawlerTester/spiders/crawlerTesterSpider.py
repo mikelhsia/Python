763d826c5759e58ec75e6b101c04671d6fedf883
mikelhsia <mikelhsia@hotmail.com>
2017-07-31 18:49:39 +0800

    Use Form request to login Mannatech page

--- pythonCrawler/crawlerTester/crawlerTester/spiders/crawlerTesterSpider.py
+++ pythonCrawler/crawlerTester/crawlerTester/spiders/crawlerTesterSpider.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
 
+class CrawlertesterspiderSpider(scrapy.Spider):
+	name = 'crawlerTesterSpider'
+	allowed_domains = ['https://meitaichina.alpha.tmogroup.asia']
+	start_urls = ['https://meitaichina.alpha.tmogroup.asia/index.php/customer/account/login/']
 
-class CrawlertesterspiderSpider(CrawlSpider):
-    name = 'crawlerTesterSpider'
-    allowed_domains = ['http://www.example.com']
-    start_urls = ['http://http://www.example.com/']
+	# rules = (Rule(LinkExtractor(allow=r'*'), callback='parse_item', follow=True), )
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+	def parse(self, response):
+		# self.logger.debug("First time in the url", response.url)
+		print "First time in the url: ", response.url
+		return scrapy.FormRequest.from_response(
+			response,
+			formdata={'username': '13817405123', 'password': '123123q'},
+			callback=self.after_login,
+			dont_filter=True
+		)
 
-    def parse_item(self, response):
-        i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+	def after_login(self, response):
+		# check login succeed before going on
+		print "Start parsing login"
+		if "authentication failed" in response.body:
+			# self.logger.error("Login failed")
+			print "Login failed"
+			return
+		else:
+			print "Login successfully"
+			print "body %s" % response.body
+		# continue scraping with authenticated session...
