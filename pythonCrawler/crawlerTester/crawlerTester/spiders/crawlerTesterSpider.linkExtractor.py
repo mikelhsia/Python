33b81f6afc64fa82ae0c3881318bfa532ae7b8c4
mikelhsia <mikelhsia@hotmail.com>
2017-08-01 18:32:54 +0800

    Add some more example from the official scrapy documentation for next step.

--- pythonCrawler/crawlerTester/crawlerTester/spiders/crawlerTesterSpider.linkExtractor.py
+++ pythonCrawler/crawlerTester/crawlerTester/spiders/crawlerTesterSpider.linkExtractor.py
@@ -11,6 +11,12 @@ class CrawlertesterspiderSpider(CrawlSpider):
 
 	rules = (
 		Rule(LinkExtractor(allow=r'com'), callback='parse_item', follow=True, ),
+		# Extract links matching 'category.php' (but not matching 'subsection.php')
+		# and follow links from them (since no callback means follow=True by default).
+		Rule(LinkExtractor(allow=('category\.php',), deny=('subsection\.php',))),
+
+		# Extract links matching 'item.php' and parse them with the spider's method parse_item
+		Rule(LinkExtractor(allow=('item\.php',)), callback='parse_item'),
 	)
 
 	def parse_item(self, response):
@@ -19,6 +25,13 @@ class CrawlertesterspiderSpider(CrawlSpider):
 		# i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
 		# i['name'] = response.xpath('//div[@id="name"]').extract()
 		# i['description'] = response.xpath('//div[@id="description"]').extract()
+
+		# self.logger.info('Hi, this is an item page! %s', response.url)
+		# item = scrapy.Item()
+		# item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+		# item['name'] = response.xpath('//td[@id="item_name"]/text()').extract()
+		# item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
+		# return item
 		return i
 
 	# If the parse function is enabled, then it would replace "parse_item" function and "parse_item" won't run
