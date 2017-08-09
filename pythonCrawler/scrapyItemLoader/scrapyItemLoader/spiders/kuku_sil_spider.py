c9378f87e78544095ade7e631ffbe9a08bdc2c33
mikelhsia <mikelhsia@hotmail.com>
2017-08-09 19:19:04 +0800

    Save the work

--- pythonCrawler/scrapyItemLoader/scrapyItemLoader/spiders/kuku_sil_spider.py
+++ pythonCrawler/scrapyItemLoader/scrapyItemLoader/spiders/kuku_sil_spider.py
@@ -36,16 +36,19 @@ class SilSpiderSpider(scrapy.Spider):
 		_xpathStr = "//a[contains(text(), \' %s%s\') and @target]/@href" % (self._targetChap, u'\u8bdd')
 		_currentChap = response.xpath(_xpathStr).extract()
 		self.logger.debug("[DEBUG] Inner url = %s", _currentChap)
-		# TODO: 除了用链接title之外，还可以用其他方法，例如去list里的第几个等等...
-		# TODO: Loop the 'targetChap' for 'numChap' times
-		# TODO: Change the response.rul to the new url
-		itemUrl = u"http://comic.kukudm.com/%s" % _currentChap[0]
+
+		# TODO: Parse the entire URL to loop through those URL in different chapters
+
+		itemUrl = u"http://comic.kukudm.com%s" % _currentChap[0]
 		self.logger.debug("[DEBUG] itemUrl = %s", itemUrl)
+
+		# TODO: Loop the 'targetChap' for 'numChap' times
 		yield Request(url=itemUrl, callback=self.parse_item, dont_filter=True)
 
 	def parse_item(self, response):
 		item = KukuComicItem()
 
+		# Parse filename and file source
 		infoScript = response.xpath('//script/text()').extract()
 		self.logger.debug("[DEBUG] Script = %s", infoScript)
 		for line in infoScript:
@@ -59,12 +62,13 @@ class SilSpiderSpider(scrapy.Spider):
 				self.logger.debug('[DEBUG] Source = %s', item['imgSrc'])
 				break
 
-		# FIXME: File Directory
+		# TODO: Rename the folder name and downloaded filename
 		dirName = response.xpath("/html/body/table[2]/tr/td[1]/text()").extract()
-		self.logger.debug("[DEBUG] DIR = %s", dirName)
-		deliIdx = dirName[0].find(' ')
-		deliEndIdx = dirName[0].find(u'\u8bdd')
-		item['imgDst'] = "%s/%s" % (os.getcwd(), dirName[0][deliIdx+1:deliEndIdx])
+		for line in dirName:
+			# self.logger.debug("[DEBUG] DIR = %s", line)
+			deliIdx = line.find(' ')
+			deliEndIdx = line.find(u'\u8bdd')
+			item['imgDst'] = "%s/%s" % (os.getcwd(), self._targetChap)
 
 		############################################################
 		# 第一页的漫画只有一个list item，所以这个是nextlink
@@ -75,17 +79,31 @@ class SilSpiderSpider(scrapy.Spider):
 		############################################################
 		try:
 			next_link = response.xpath("/html/body/table[2]/tr/td/a[2]/@href").extract()
+			self.logger.debug("[TESTING]: Next link - %s", next_link)
 			if '/exit/exit.htm' not in next_link:
+				self.logger.debug("[TESTING]: not exit.htm")
 				# Meaning this is not the last page
 				if not next_link:
 					# Meaning this is the first page, so using the first link xpath instead of the second one
-					next_link = response.xpath("/html/body/table[2]/tr/td/a[1]/@href").extract()
-					self.logger.debug("[TESTING]: Next link url - %s", next_link)
+					next_link = response.xpath("/html/body/table[2]/tr/td/a[1]/@href").extract()[0]
+					# self.logger.debug("[TESTING]: Next link url - %s", next_link)
+					# self.logger.debug("[TESTING]: Response url - %s", response.url)
+					# self.logger.debug("[TESTING]: Final url - %s", urlparse.urljoin(response.url, next_link))
+				else:
+					next_link = next_link[0]
+					# self.logger.debug("[TESTING]: Next Link is not empty")
+					# self.logger.debug("[TESTING]: Next link url - %s", next_link)
+					# self.logger.debug("[TESTING]: Response url - %s", response.url)
+					# self.logger.debug("[TESTING]: Final url - %s", urlparse.urljoin(response.url, next_link))
+					pass
 				yield Request(url=urlparse.urljoin(response.url, next_link), callback=self.parse_item, dont_filter=True)
 			else:
-				self.logger.debug("[TESTING]: Else")
+				self.logger.debug("[TESTING]:exit.htm")
 				pass
 
 		except:
-			self.logger.debug("[TESTING]: Something went wrong")
+			self.logger.error("[TESTING]: Something went wrong")
+			pass
+
+		yield item
 
