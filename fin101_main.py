2c39ee78022a00de46dfd6c3f602e0269f8d76ea
mikelhsia <mikelhsia@hotmail.com>
2017-09-13 00:01:15 +0800

    [Python for Finance] 1. Using colors effectively 2. xerr, yerr ecolor error bar

--- fin101_main.py
+++ fin101_main.py
@@ -275,3 +275,39 @@ plt.plot(rate, npv), plt.plot(x, y)
 if input("Want to show NPV-IRR profile?") == 'y': plt.show()
 
 plt.clf()
+# Using colors effectively
+A_EPS = (5.02, 4.54, 4.18, 3.73)
+B_EPS = (1.35, 1.88, 1.35, 0.73)
+
+A_STD = sp.std(A_EPS) * 1.5
+B_STD = sp.std(B_EPS) * 1.5
+
+# the X locations for the groups
+ind = np.arange(len(A_EPS))
+# the width of the bars
+width = 0.4
+
+fig, ax = plt.subplots()
+'''
+yerr : scalar or array-like, optional (y的误差棒图)
+	if not None, will be used to generate errorbar(s) on the bar chart
+	default: None
+'''
+rects1 = ax.bar(ind, A_EPS, width, color='r', yerr=A_STD, ecolor='b')
+rects2 = ax.bar(ind+width, B_EPS, width, color='y', yerr=B_STD, ecolor='g')
+ax.set_ylabel('EPS')
+ax.set_xlabel("Year")
+ax.set_title("Diluted EPS Excluding Extraordinary Items")
+ax.set_xticks(ind+width)
+ax.set_xticklabels(('2012', '2011', '2010', '2009'))
+ax.legend((rects1[0], rects2[0]),('W-Mart', 'DELL'))
+
+def autolabel(rects):
+	for rect in rects:
+		height = rect.get_height()
+		ax.text(rect.get_x() + rect.get_width()/2., 1.05*height, '%d' % int(height), ha='center', va='bottom')
+
+autolabel(rects1)
+autolabel(rects2)
+
+if input("Want to show EPS color graphs?") == 'y': plt.show()
