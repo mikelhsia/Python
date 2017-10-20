b66246ce72d1461478f24122e40b48f2b391fd35
mikelhsia <mikelhsia@hotmail.com>
2017-10-20 18:16:20 +0800

    Python Django Practice: 4. database 4.1 Using sqlite3 come with default Django 4.2 curl the data in the database

--- practiceDJ/practiceDJ/testdb.py
+++ practiceDJ/practiceDJ/testdb.py
@@ -14,31 +14,32 @@ def testdb(request):
 	# 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
 	list = Test.objects.all()
 
-	# filter相当于SQL中的WHERE，可设置条件过滤结果
-	# response2 = Test.objects.filter(id=1)
+	if len(list) < 1:
+		# filter相当于SQL中的WHERE，可设置条件过滤结果
+		# response2 = Test.objects.filter(id=1)
 
-	# 获取单个对象
-	# response3 = Test.objects.get(id=1)
+		# 获取单个对象
+		# response3 = Test.objects.get(id=1)
 
-	# 限制返回的数据 相当于 SQL 中的 OFFSET 0 LIMIT 2;
-	response4 = Test.objects.order_by('username')[0:2]
+		# 限制返回的数据 相当于 SQL 中的 OFFSET 0 LIMIT 2;
+		response4 = Test.objects.order_by('username')[0:2]
 
-	# 数据排序
-	response5 = Test.objects.order_by("id")
+		# 数据排序
+		response5 = Test.objects.order_by("id")
 
-	# 上面的方法可以连锁使用
-	response6 = Test.objects.filter(username="runoob").order_by("id")
+		# 上面的方法可以连锁使用
+		response6 = Test.objects.filter(username="runoob").order_by("id")
 
-	# Modify/Update data
-	test2 = Test.objects.get(id=test1.id-1)
-	test2.username = "runxxb"
-	test2.save()
+		# Modify/Update data
+		test2 = Test.objects.get(id=test1.id-1)
+		test2.username = "runxxb"
+		test2.save()
 
-	# 另外一种方式
-	# Test.objects.filter(id=1).update(name='Google')
+		# 另外一种方式
+		# Test.objects.filter(id=1).update(name='Google')
 
-	# 修改所有的列
-	# Test.objects.all().update(username='runxxb')
+		# 修改所有的列
+		# Test.objects.all().update(username='runxxb')
 
 	for var in list:
 		response1 += var.username + "({}), ".format(var.id)
