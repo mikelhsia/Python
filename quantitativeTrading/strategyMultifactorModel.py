39ea4127296fcaafa262d46fcc3304f2cb423942
mikelhsia <mikelhsia@hotmail.com>
2017-11-23 00:01:41 +0800

    [Python Quantitative Trading] 1. Multifactor Modeling second commit, not doing much

--- quantitativeTrading/strategyMultifactorModel.py
+++ quantitativeTrading/strategyMultifactorModel.py
@@ -5,38 +5,52 @@ Author: Michael Hsia
 Description:
 """
 import numpy as np
-import ts2numpy as ts2np
+import tushare as ts
 import datetime
 
 """
 Tips:
+使用回望交易日作为估计区间（训练集），以此来决定因子风险
+回望期交易日为252天，因子5个
+交易策略为：购买下一个交易日期望收益率最高的20只股票
 
 Terms:
 """
 
+topN = 20
+
+def _getHS300Tickers():
+	tickers = ts.get_hs300s()
+	return tickers['code']
+
 def main():
-	oilStockLabel = 1
-	bankStockLabel = 2
-	retailStockLabel = 3
-	tickers = ['601857', '601318', '300033']
-	begDate = "2016-11-01"
-	endDate = str(datetime.date.today())
-	dataSet = None
 
-	np.set_printoptions(threshold=10)   # Adjust print columns number to 10
+	hs300Tickers = _getHS300Tickers()
 
-	for ticker in tickers:
-		ticketClosePrice = ts2np.ts2numpy_dohcl(ticker, begDate, endDate)
-		if dataSet is None:
-			# Actually we don't need the date
-			# dataSet = np.array(ticketClosePrice[0])
-			dataSet = np.array(ticketClosePrice[3])
-		else:
-			# dataSet = np.array([dataSet, ticketClosePrice[3]])
-			dataSet = np.row_stack((dataSet, ticketClosePrice[3]))
-	else:
-		print("Done combining three stocks into one array!")
-		# print(dataSet)
+	# begDate = "2016-11-01"
+	# endDate = str(datetime.date.today())
+	# dataSet = None
+	#
+	# np.set_printoptions(threshold=10)   # Adjust print columns number to 10
+
+	for ticker in hs300Tickers:
+		print(ticker)
+
+	# 	ticketClosePrice = ts2np.ts2numpy_dohcl(ticker, begDate, endDate)
+	# 	if dataSet is None:
+	# 		# Actually we don't need the date
+	# 		# dataSet = np.array(ticketClosePrice[0])
+	# 		dataSet = np.array(ticketClosePrice[3])
+	# 	else:
+	# 		# dataSet = np.array([dataSet, ticketClosePrice[3]])
+	# 		dataSet = np.row_stack((dataSet, ticketClosePrice[3]))
+	# else:
+	# 	print("Done combining three stocks into one array!")
+	# 	# print(dataSet)
 
 	# 收益率
-	ret = np.array((dataSet[:,1:] - dataSet[:,:-1]) / dataSet[:,:-1]
+	# ret = np.array((dataSet[:, 1:] - dataSet[:, :-1]) / dataSet[:, :-1])
+
+
+if __name__ == "__main__":
+	main()
