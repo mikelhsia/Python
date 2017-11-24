2b13a96fb8e419a26f93f300da3e443721194f05
mikelhsia <mikelhsia@hotmail.com>
2017-11-24 12:26:14 +0800

    [Python Quantitative Trading] 1. Pandas DataFrame handling data 2. pd.concating close price from tushare in different columns 3. Rename those columns as stock ticker

--- quantitativeTrading/strategyMultifactorModel.py
+++ quantitativeTrading/strategyMultifactorModel.py
@@ -7,6 +7,7 @@ Description:
 import numpy as np
 import tushare as ts
 import datetime
+import pandas as pd
 
 """
 Tips:
@@ -27,26 +28,29 @@ def main():
 
 	hs300Tickers = _getHS300Tickers()
 
-	# begDate = "2016-11-01"
-	# endDate = str(datetime.date.today())
-	# dataSet = None
-	#
+	begDate = "2016-11-01"
+	endDate = datetime.datetime.timestamp(datetime.datetime.today())
+	lookback = 30
+
 	# np.set_printoptions(threshold=10)   # Adjust print columns number to 10
 
 	for ticker in hs300Tickers:
-		print(ticker)
+		hisPrice = ts.get_hist_data(ticker, begDate, datetime.datetime.fromtimestamp(endDate).__str__())
 
-	# 	ticketClosePrice = ts2np.ts2numpy_dohcl(ticker, begDate, endDate)
-	# 	if dataSet is None:
-	# 		# Actually we don't need the date
-	# 		# dataSet = np.array(ticketClosePrice[0])
-	# 		dataSet = np.array(ticketClosePrice[3])
-	# 	else:
-	# 		# dataSet = np.array([dataSet, ticketClosePrice[3]])
-	# 		dataSet = np.row_stack((dataSet, ticketClosePrice[3]))
-	# else:
-	# 	print("Done combining three stocks into one array!")
-	# 	# print(dataSet)
+		'''
+		Another way of implementing this
+			try:
+				print('Found: {0}'.format())
+			except NameError:
+				print('Not found')
+			else:
+		'''
+		if 'tickerPrices' not in locals().keys():
+			tickerPrices = pd.DataFrame(hisPrice['close'].rename(ticker), index=hisPrice.index)
+		else:
+			tickerPrices = pd.concat([tickerPrices, hisPrice['close'].rename(ticker)], axis=1)
+
+	print(tickerPrices)
 
 	# 收益率
 	# ret = np.array((dataSet[:, 1:] - dataSet[:, :-1]) / dataSet[:, :-1])
