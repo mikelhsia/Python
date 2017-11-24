b1b13b4fc6cc3a1316b77fc28f910806015ea02d
mikelhsia <mikelhsia@hotmail.com>
2017-11-24 17:54:05 +0800

    [Python Quantitative Trading] 1. Establish all-zeros position table 2. Creating testing flag to conduct test faster 3. Calculate the daily return by using "df.values" to add two DataFrames together 4. Implementing lookback to start experimenting

--- quantitativeTrading/strategyMultifactorModel.py
+++ quantitativeTrading/strategyMultifactorModel.py
@@ -17,6 +17,7 @@ Tips:
 
 Terms:
 """
+TESTING_FLAG = True
 
 topN = 20
 
@@ -30,11 +31,14 @@ def main():
 
 	begDate = "2016-11-01"
 	endDate = datetime.datetime.timestamp(datetime.datetime.today())
-	lookback = 30
+	lookback = 30		# 回望期
 
 	# np.set_printoptions(threshold=10)   # Adjust print columns number to 10
+	if TESTING_FLAG == True:
+		i = 0
 
 	for ticker in hs300Tickers:
+
 		hisPrice = ts.get_hist_data(ticker, begDate, datetime.datetime.fromtimestamp(endDate).__str__())
 
 		'''
@@ -45,12 +49,32 @@ def main():
 				print('Not found')
 			else:
 		'''
-		if 'tickerPrices' not in locals().keys():
-			tickerPrices = pd.DataFrame(hisPrice['close'].rename(ticker), index=hisPrice.index)
+		if 'tickerPriceTable' not in locals().keys():
+			tickerPriceTable = pd.DataFrame(hisPrice['close'].rename(ticker), index=hisPrice.index)
 		else:
-			tickerPrices = pd.concat([tickerPrices, hisPrice['close'].rename(ticker)], axis=1)
+			tickerPriceTable = pd.concat([tickerPriceTable, hisPrice['close'].rename(ticker)], axis=1)
 
-	print(tickerPrices)
+		if TESTING_FLAG == True:
+			i += 1
+			if i > 4:
+				break
+
+	print(tickerPriceTable)
+
+	# 建立全0的position table
+	positionTable = pd.DataFrame(np.zeros(shape=(len(tickerPriceTable.index), len(tickerPriceTable.columns))), 
+		                        index=tickerPriceTable.index, 
+		                        columns=tickerPriceTable.columns)
+
+	dailyRet = pd.DataFrame((tickerPriceTable.values[1:,:] - tickerPriceTable.values[:-1,:]) / tickerPriceTable.values[:-1,:], columns=tickerPriceTable.columns)
+
+	for d in range(lookback+1, len(tickerPriceTable.index)):
+		# R 的列是不同的观测现象
+		R = dailyRet.ix[(d - lookback + 1):d,:]
+
+		if TESTING_FLAG == True:
+			print(R)
+			break
 
 	# 收益率
 	# ret = np.array((dataSet[:, 1:] - dataSet[:, :-1]) / dataSet[:, :-1])
