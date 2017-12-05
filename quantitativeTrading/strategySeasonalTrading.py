33f817a0f11727a0c2e8981aa37c7b293b0f33ec
mikelhsia <mikelhsia@hotmail.com>
2017-12-05 17:27:20 +0800

    [Python Quantitative Trading] 1. Remove meaningless local function 2. Adjust the format of function use to apply PEP8 3. Implement the way to select data needed

--- quantitativeTrading/strategySeasonalTrading.py
+++ quantitativeTrading/strategySeasonalTrading.py
@@ -20,10 +20,6 @@ TESTING_FLAG = True
 TESTING_SCOPE = 3
 
 
-def _getHS300Tickers():
-	tickers = ts.get_hs300s()
-	return tickers['code']
-
 def main():
 
 	oneWayTransCost = 0.0005    # 5bp one way transaction cost
@@ -31,11 +27,11 @@ def main():
 	begDate = "2013-11-01"
 	today = datetime.date.today()
 
-	hs300Tickers = _getHS300Tickers()
+	hs300Tickers = ts.get_hs300s()
 	if TESTING_FLAG:
 		i = 0
 
-	for ticker in hs300Tickers:
+	for ticker in hs300Tickers['code']:
 
 		hisPrice = ts.get_hist_data(ticker, begDate, today.__str__())
 
@@ -51,21 +47,29 @@ def main():
 				break
 
 	dateIndex = pd.DataFrame(tickerPriceTable.index, index=tickerPriceTable.index)
-	years = dateIndex.applymap(lambda x:datetime.datetime.strptime(x, "%Y-%m-%d").year)
-	years = years.iloc[:,0].rename('years')
-	months = dateIndex.applymap(lambda x:datetime.datetime.strptime(x, "%Y-%m-%d").month)
-	months = months.iloc[:,0].rename('months')
-	nextDayYear = dateIndex.applymap(lambda x:(datetime.datetime.strptime(x, "%Y-%m-%d") + datetime.timedelta(days=1)).year)
-	nextDayYear = nextDayYear.iloc[:,0].rename('nextDayYear')
-	nextDayMonth = dateIndex.applymap(lambda x:(datetime.datetime.strptime(x, "%Y-%m-%d") + datetime.timedelta(days=1)).month)
-	nextDayMonth = nextDayMonth.iloc[:,0].rename('nextDayMonth')
+	years = dateIndex.applymap(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").year)
+	years = years.iloc[:, 0].rename('years')
+	months = dateIndex.applymap(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").month)
+	months = months.iloc[:, 0].rename('months')
+	nextDayYear = dateIndex.applymap(lambda x: (datetime.datetime.strptime(x, "%Y-%m-%d") + datetime.timedelta(days=1)).year)
+	nextDayYear = nextDayYear.iloc[:, 0].rename('nextDayYear')
+	nextDayMonth = dateIndex.applymap(lambda x: (datetime.datetime.strptime(x, "%Y-%m-%d") + datetime.timedelta(days=1)).month)
+	nextDayMonth = nextDayMonth.iloc[:, 0].rename('nextDayMonth')
 
 	dateTable = pd.concat([years, months, nextDayYear, nextDayMonth], axis=1)
-	print(dateTable)
-	print(type(dateTable))
-	# lastDayOfDec = pd.DataFrame(months == 12)
-	# lastDayOfJan = pd.DataFrame(months == 1)
+	# print(dateTable)
+	# print(type(dateTable))
+
+	lastDayOfDec = dateTable[(dateTable['months'] == 12) & (dateTable['nextDayMonth'] == 1)]
+	# 用where：
+	# The where method is an application of the if-then idiom. For each element in the calling DataFrame,
+	# if cond is True the element is used; otherwise the corresponding element from the DataFrame other is used
+	# lastDayOfDec = dateTable.where((dateTable['months'] == 12) & (dateTable['nextDayMonth'] == 1))
+	lastDayOfJan = dateTable[(dateTable['months'] == 1) & (dateTable['nextDayMonth'] == 2)]
 	# print(lastDayOfDec)
+
+	# 由于lastDayOfDec 从第一年开始，也就是代表在第一年开始执行策略的时候，并没有前一年的股价完整资料
+	lastDayOfDec.loc[0, :] = np.NaN
 
 
 if __name__ == "__main__":
