2b232652e637553d4b93be54665e73f2743e24e0
mikelhsia <mikelhsia@hotmail.com>
2017-12-06 18:04:10 +0800

    [Python Quantitative Trading] 1. Concat tickerPriceTable and dateTable, to make sure all data on the same table 2. Calculation of eoy, firstDayOfJan

--- quantitativeTrading/strategySeasonalTrading.py
+++ quantitativeTrading/strategySeasonalTrading.py
@@ -56,21 +56,40 @@ def main():
 	nextDayMonth = dateIndex.applymap(lambda x: (datetime.datetime.strptime(x, "%Y-%m-%d") + datetime.timedelta(days=1)).month)
 	nextDayMonth = nextDayMonth.iloc[:, 0].rename('nextDayMonth')
 
-	dateTable = pd.concat([years, months, nextDayYear, nextDayMonth], axis=1)
-	# print(dateTable)
-	# print(type(dateTable))
+	tickerPriceTable = pd.concat([tickerPriceTable, years, months, nextDayYear, nextDayMonth], join='inner', axis=1)
+	# print("Ticker price: \n{}".format(tickerPriceTable))
 
-	lastDayOfDec = dateTable[(dateTable['months'] == 12) & (dateTable['nextDayMonth'] == 1)]
+	lastDayOfDec = tickerPriceTable[(tickerPriceTable['months'] == 12) & (tickerPriceTable['nextDayMonth'] == 1)]
 	# 用where：
 	# The where method is an application of the if-then idiom. For each element in the calling DataFrame,
 	# if cond is True the element is used; otherwise the corresponding element from the DataFrame other is used
 	# lastDayOfDec = dateTable.where((dateTable['months'] == 12) & (dateTable['nextDayMonth'] == 1))
-	lastDayOfJan = dateTable[(dateTable['months'] == 1) & (dateTable['nextDayMonth'] == 2)]
+	lastDayOfJan = tickerPriceTable[(tickerPriceTable['months'] == 1) & (tickerPriceTable['nextDayMonth'] == 2)]
 	# print(lastDayOfDec)
 
 	# 由于lastDayOfDec 从第一年开始，也就是代表在第一年开始执行策略的时候，并没有前一年的股价完整资料
 	lastDayOfDec.loc[0, :] = np.NaN
 
+	# 年末指数
+	eoy = tickerPriceTable[tickerPriceTable['years'] == tickerPriceTable['nextDayYear']]
+	# print("EOY:\n{}".format(eoy))
+
+	# 上一个指数不是年末的
+	eoy.iloc[-1, :] = np.NaN
+
+	# 确保eoy日期和lastDayOfDec日期匹配
+	# assert(all(tday(eoy) == tday(lastDayOfDec)))
+
+	# 年收益率
+	annRet = pd.DataFrame((eoy.values[1:,:] - eoy.values[:-1,:]) / eoy.values[:-1,:], columns=eoy.columns)
+	# print(annRet)
+
+	# 年收益率
+	# janRet = pd.DataFrame((lastDayOfJan.values[1:,:] - lastDayOfDec.values[:-1,:]) / lastDayOfDec.values[:-1,:], columns=eoy.columns)
+	# print(annRet)
+
+	for index, stock in enumerate(annRet):
+		print("{}, {}".format(index, stock))
 
 if __name__ == "__main__":
 	main()
