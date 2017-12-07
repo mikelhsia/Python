5797c6f7526ebe3ecc392d82046eeba827c9082f
mikelhsia <mikelhsia@hotmail.com>
2017-12-07 23:53:12 +0800

    [Python Quantitative Trading] 1. strategyAnnualSeasonalTrading.py first implementation

--- quantitativeTrading/strategyAnnualSeasonalTrading.py
+++ quantitativeTrading/strategyAnnualSeasonalTrading.py
@@ -8,19 +8,54 @@ import numpy as np
 import tushare as ts
 import datetime
 import pandas as pd
-from numpy.matlib import repmat
 
 """
 Tips:
 
 Terms:
 """
+
 TESTING_FLAG = True
-TESTING_SCOPE = 10
+TESTING_SCOPE = 3
 
 def main():
-	return
 
+	if TESTING_FLAG:
+		i = 0
+
+	begDate = "2013-11-01"
+	today = datetime.date.today()
+
+	hs300Tickers = ts.get_hs300s()
+
+	for ticker in hs300Tickers['code']:
+
+		hisPrice = ts.get_hist_data(ticker, begDate, today.__str__())
+
+		if 'tickerPriceTable' not in locals().keys():
+			tickerPriceTable = pd.DataFrame(hisPrice['close'].rename(ticker), index=hisPrice.index)
+		else:
+			tickerPriceTable = pd.concat([tickerPriceTable, hisPrice['close'].rename(ticker)], axis=1)
+
+		if TESTING_FLAG:
+			i += 1
+			if i > TESTING_SCOPE:
+				# print(tickerPriceTable)
+				break
+
+	# 将日期index变成其中一个栏位
+	tickerPriceTable = tickerPriceTable.reset_index()
+
+	# 找到月底几天的指数
+	monthEnds = tickerPriceTable[isLastTradingDayOfMonth(tickerPriceTable.index)]
+
+def isLastTradingDayOfMonth(index):
+	'''
+	Identify whether the trading day is the last trading day of the month
+	:param index: Trading date
+	:return: boolean True or False
+	'''
+	pass
 
 if __name__ == "__main__":
 	main()
