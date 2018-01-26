dd07a750f9f8dd48a18870db3c1f22691b8a6c59
mikelhsia <mikelhsia@hotmail.com>
2018-01-26 22:31:58 +0800

    [Stock Analysis] 1. Add further modification and plot

--- quantitativeTrading/stockResearch/stockFundamentalResearch.py
+++ quantitativeTrading/stockResearch/stockFundamentalResearch.py
@@ -8,10 +8,12 @@ import tushare as ts
 import numpy as np
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
 
 
-def main():
-    df = ts.get_balance_sheet('600518') 
+def get_funda_finance(security):
+    # df = ts.get_balance_sheet('000001') 
+    df = ts.get_cash_flow(security) 
     list = df.报表日期.tolist()
     
     list.reverse()
@@ -31,7 +33,29 @@ def main():
     dfT.set_index(dfT.报表日期, inplace=True)  
     dfT.drop('报表日期', axis=1, inplace=True)   
 
-    print(dfT)
+    dfT.sort_index(ascending=True, inplace=True)
+
+#     print(dfT)
+#     print(dfT.columns)
+    return df
+
+def main():
+    df = get_funda_finance('000625')
+    a = df.经营活动现金流入小计.map(lambda x: float(x))
+    b = df.经营活动现金流入小计.map(lambda x: 0)
+
+    fig, axes = plt.subplots(1, 1)
+
+    a = a.pct_change()
+
+    p = a[a > 0]
+    n = a[a < 0]
+    a.plot(kind='bar')
+    # a.plot()
+    # b.plot()
+
+    # n.plot(kind='bar')
+    # p.plot(kind='bar')
 
 if __name__ == '__main__':
     main()
