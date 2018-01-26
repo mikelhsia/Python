633dcb836d45a1d07fc04919683d280bda2b8d67
mikelhsia <mikelhsia@hotmail.com>
2018-01-26 20:20:48 +0800

    [Stock Analysis] 1. Switch date string to date format and set it to index.

--- quantitativeTrading/stockResearch/stockFundamentalResearch.py
+++ quantitativeTrading/stockResearch/stockFundamentalResearch.py
@@ -26,6 +26,11 @@ def main():
 
     dfT.rename(columns=dic, inplace=True)
     dfT.drop(0, inplace=True)  
+
+    dfT.报表日期 = dfT.报表日期.map(lambda x: datetime.datetime.strptime(x, '%Y%m%d'))
+    dfT.set_index(dfT.报表日期, inplace=True)  
+    dfT.drop('报表日期', axis=1, inplace=True)   
+
     print(dfT)
 
 if __name__ == '__main__':
