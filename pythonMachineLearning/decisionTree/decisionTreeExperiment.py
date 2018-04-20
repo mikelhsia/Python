ac4e9a2bc474ba1e51a99b6f421cb70fd317966b
Tsu Yu Hsia <tsuyuhsia@Admins-Air-4.tmogroup.wan>
2018-04-20 17:01:46 +0800

    [Stock Analysis] 1. create function for simpify getting bins

--- pythonMachineLearning/decisionTree/decisionTreeExperiment.py
+++ pythonMachineLearning/decisionTree/decisionTreeExperiment.py
@@ -13,20 +13,28 @@ import matplotlib.pyplot as plt
 
 import tushare as ts
 
-response = ['change_rate_quant']
+target = ['change_rate_quant']
 predictors = ['PCF', 'turnover_ratio', 'PE_rate', 'PB_rate', 'PS_rate', 'pmi', 'pmi_rate', 'leading_idx_rate', 'M1_rate',
               'M2_rate', 'CPI_rate', 'GDP_rate', 'LOAN6MONTH', 'LARGEFINANCIAL', 'SMALLFINANCIAL', 'dcg', 'URUR']
+
+def get_bins (srs, frac= []):
+    arr = []
+
+    if (frac == []) :
+        raise Exception('frac array is empty. Please specify quantile detail');
+    bn = srs.quantile([0.3, 0.7])
+    arr = [pd.Series.min(srs)] + [bn.iloc[x] for x in range(pd.Series.count(bn))] + [pd.Series.max(srs)]
+    return arr
 
 df = ts.get_hist_data('600848')
 
 # TODO: Moving windows and rolling
 df['change_rate'] = df['close'].pct_change()
-bin = df['change_rate'].quantile([0.3, 0.7])
-bins = [pd.Series.min(df['change_rate']), bin.iloc[0], bin.iloc[1], pd.Series.max(df['change_rate'])]
-cats = pd.cut(df['change_rate'], bins)
+cats = pd.cut(df['change_rate'], get_bins(df['change_rate'], [0.25, 0.75]))
 
 # print(cats.values)
 # print(cats.values.codes)
 # print(cats.values.categories)
 
-df['change_rate_qunat'] = cats.values.codes
+df['change_rate_quant'] = cats.values.codes
+print(df['change_rate_quant'])
