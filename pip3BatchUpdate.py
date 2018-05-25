b7001950747578d4b9759be7e22154c5a241ec0a
Michael <michael@MichaeldeMacBook-Pro.local>
2018-05-25 17:52:59 +0800

    [Misc] 1. Change from using pip lib to pkg_resources. 2. It was mentioned somewhere that it's safer

--- pip3BatchUpdate.py
+++ pip3BatchUpdate.py
@@ -1,5 +1,5 @@
-import pip
+import pkg_resources
 from subprocess import call
  
-for dist in pip.get_installed_distributions():
+for dist in [d for d in pkg_resources.working_set]:
     call("pip3 install --upgrade " + dist.project_name, shell=True)
