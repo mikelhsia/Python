0b3287ebf463ac644910c961f2e93cbbf6273509
Michael <michael@MichaeldeMacBook-Pro.local>
2018-06-07 17:33:03 +0800

    [Python Tools - Timehut scrapy] 1. Separate data and function process 2. loop the request

--- pythonTools/getTimeHut.py
+++ pythonTools/getTimeHut.py
@@ -1,3 +1,41 @@
 import requests
-r = requests.get('https://www.douban.com/')
-r.status_code
+import json
+
+import getTimehutData
+
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# functions
+def getRequest(baby_id, before_day):
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Cookie': 'locale=en;user_session=BAhJIj1qcF81MzY5MjMzNjNfM0JyZDJlNV9LbkJ1OGtqdkRqSHM4UmZyVk1CVUk4a3BrY1JRME9ZVzc1dwY6BkVU--fcb138d8ae1fcb3290cbbd7c4b35101f61d8b40e'
+    }
+
+    r = requests.get(url='http://peekaboomoments.com/events.json?baby_id={}&before={}&v=2&width=700&include_rt=true'.format(baby_id, before_day), headers=headers)
+
+    response_body = json.loads(r.text)
+#    print(response_body)
+    print("Request fired = {}".format(response_body['next']))
+    return response_body['next']
+
+def createEngine():
+    engine = create_engine('mysql+pymysql://root:michael0512@127.0.0.1:3306/php_test')
+
+
+# main function()
+def main():
+    before_day = 50
+    baby_id = 537413380
+    next_index = 0
+    while (next_index is not None):
+        before_day = next_index + 2
+        nex_index = getRequest(baby_id, before_day)
+#    createEngine()
+    getTimehutData.printabc()
+
+# check
+if __name__ == "__main__":
+    main()
--- pythonTools/getTimehutData.py
+++ pythonTools/getTimehutData.py
@@ -1,46 +1,30 @@
-import requests
-import json
-
-from sqlalchemy import Column, String, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
 class MemoryCollection:
-	def __init__(self, id, baby_id, created_at, months, days, layout, caption, obj):
-		self.id = id
-		self.baby_id = baby_id
-		self.created_at = created_at
-		self.months = months
-		self.days = days
-		self.layout = layout
-		self.layout_id_list = []
-		self.caption = caption
-		self.memory = Memory(obj)
+    def __init__(self, id, baby_id, created_at, months, days, layout, caption, obj):
+        self.id = id
+        self.baby_id = baby_id
+        self.created_at = created_at
+        self.months = months
+        self.days = days
+        self.layout = layout
+        self.layout_id_list = []
+        self.caption = caption
+        self.memory = Memory(obj)
 
-	def store(self):
-		pass
+    def store(self):
+        pass
+
 
 class Memory:
-	def __init__(self, id, content, photo_path, video_path):
-		self.id = id
-		self.content = content
-		self.src_url = video_path if video_path else photo_path
+    def __init__(self, id, content, photo_path, video_path):
+        self.id = id
+        self.content = content
+        self.src_url = video_path if video_path else photo_path
 
-	def store(self):
-		pass
+    def store(self):
+        pass
 
 memoryCollectionSet = set()
 memorySet = set()
 
-before_day = 3000
-
-headers = {
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-    'Cookie': 'locale=en;user_session=BAhJIj1qcF81MzY5MjMzNjNfM0JyZDJlNV9LbkJ1OGtqdkRqSHM4UmZyVk1CVUk4a3BrY1JRME9ZVzc1dwY6BkVU--fcb138d8ae1fcb3290cbbd7c4b35101f61d8b40e'
-}
-
-r = requests.get(url='http://peekaboomoments.com/events.json?baby_id=537413380&before={}&v=2&width=700&include_rt=true'.format(before_day), headers=headers)
-
-obj_list = json.loads(r.text)['list']
-
-engine = create_engine('mysql+pymysql://root:michael0512@127.0.0.1:3306/php_test')
+def printabc():
+    print('---------------')
