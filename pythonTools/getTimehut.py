9f10b5298660e1e95a172e095f8bf89062b63b7c
Michael <michael@MichaeldeMacBook-Pro.local>
2018-06-24 21:52:28 +0800

    [Python Tools - Timehut scrapy] 1, Extract log as a stand-alone log module 2. Add last update time param into interactive interface 3. Make a index list to generate the index of existing record in database 4. Combine the logic of insert and update collection/moment as one single function

--- pythonTools/getTimehut.py
+++ pythonTools/getTimehut.py
@@ -1,17 +1,13 @@
 import requests
-import logging
 import json
 from datetime import datetime
-
-import timehutDataSchema
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-logging.basicConfig(filename='log1.log',
-					format='%(asctime)s -%(name)s-%(levelname)s-%(module)s:%(message)s',
-					datefmt='%Y-%m-%d %H:%M:%S %p',
-					level=logging.DEBUG)
+import timehutDataSchema
+import timehutQueue
+import timehutLog
 
 # functions
 def timestampToDatetimeString(ts):
@@ -67,10 +63,10 @@ def getCollectionRequest(baby_id, before_day):
 		r = requests.get(url=f'http://peekaboomoments.com/events.json?baby_id={baby_id}&before={before_day}&v=2&width=700&include_rt=true', headers=headers, timeout=30)
 		r.raise_for_status()
 	except requests.RequestException as e:
-		logging.error(e)
+		timehutLog.logging.error(e)
 	else:
 		response_body = json.loads(r.text)
-		logging.info(f"Request fired = {response_body['next']}")
+		timehutLog.logging.info(f"Request fired = {response_body['next']}")
 		return response_body['next'], response_body
 
 def getMomentRequest(collection_id):
@@ -89,7 +85,7 @@ def getMomentRequest(collection_id):
 		r = requests.get(url=f'http://peekaboomoments.com/events/{collection_id}', headers=headers, timeout=30)
 		r.raise_for_status()
 	except requests.RequestException as e:
-		logging.error(e)
+		timehutLog.logging.error(e)
 	else:
 		response_body = json.loads(r.text)
 		return response_body
@@ -117,7 +113,7 @@ def parseCollectionBody(response_body):
 
 			# Add to return collection obj list
 			collection_list.append(c_rec)
-			print(c_rec)
+			# print(c_rec)
 
 		elif data['layout'] == 'milestone':
 			continue
@@ -153,7 +149,7 @@ def parseMomentBody(response_body):
 
 		# Add to return collection obj list
 		moment_list.append(m_rec)
-		print(m_rec)
+		# print(m_rec)
 
 	return moment_list
 
@@ -162,27 +158,27 @@ def createDB(dbName, base, loggingFlag):
 	                       encoding='utf-8', echo=loggingFlag)
 
 	engine.execute(f"CREATE DATABASE IF NOT EXISTS {dbName} DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;")
-	logging.info(f"CREATE DATABASE IF NOT EXISTS {dbName} DEFAULT CHARSET utf8mb4 COLLATE utf8_general_ci;")
+	timehutLog.logging.info(f"CREATE DATABASE IF NOT EXISTS {dbName} DEFAULT CHARSET utf8mb4 COLLATE utf8_general_ci;")
 
 	engine.execute(f"USE {dbName}")
-	logging.info(f"USE {dbName}")
+	timehutLog.logging.info(f"USE {dbName}")
 
 
 	# TODO: It's a stupid way to drop the table everytime. Needs improvement
 	# TODO: Make sure the second call won't drop the previous table and update the old ones
-	engine.execute(f"DROP TABLE {timehutDataSchema.Moment.__tablename__}")
-	logging.info(f"DROP TABLE {timehutDataSchema.Moment.__tablename__}")
+	# engine.execute(f"DROP TABLE {timehutDataSchema.Moment.__tablename__}")
+	# timehutLog.logging.info(f"DROP TABLE {timehutDataSchema.Moment.__tablename__}")
 
-	engine.execute(f"DROP TABLE {timehutDataSchema.Collection.__tablename__}")
-	logging.info(f"DROP TABLE {timehutDataSchema.Collection.__tablename__}")
+	# engine.execute(f"DROP TABLE {timehutDataSchema.Collection.__tablename__}")
+	# timehutLog.logging.info(f"DROP TABLE {timehutDataSchema.Collection.__tablename__}")
 	
-	base.metadata.create_all(engine)
+	# base.metadata.create_all(engine)
 
-	engine.execute(f"ALTER DATABASE {dbName} CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;")
-	engine.execute(f"ALTER TABLE {timehutDataSchema.Collection.__tablename__} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
-	engine.execute(f"ALTER TABLE {timehutDataSchema.Collection.__tablename__} MODIFY caption TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
-	engine.execute(f"ALTER TABLE {timehutDataSchema.Moment.__tablename__} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
-	engine.execute(f"ALTER TABLE {timehutDataSchema.Moment.__tablename__} MODIFY content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+	# engine.execute(f"ALTER DATABASE {dbName} CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;")
+	# engine.execute(f"ALTER TABLE {timehutDataSchema.Collection.__tablename__} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
+	# engine.execute(f"ALTER TABLE {timehutDataSchema.Collection.__tablename__} MODIFY caption TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+	# engine.execute(f"ALTER TABLE {timehutDataSchema.Moment.__tablename__} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
+	# engine.execute(f"ALTER TABLE {timehutDataSchema.Moment.__tablename__} MODIFY content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
 
 	engine.dispose()
 
@@ -196,18 +192,82 @@ def createEngine(dbName, base, loggingFlag):
 
 	return engine
 
+def generateIndexList(engine):
+	DBSession = sessionmaker(bind=engine)
+	__session = DBSession()
+
+	__collectionIndexList = []
+	__momentIndexList = []
+
+	for row in __session.query(timehutDataSchema.Collection):
+		__collectionIndexList.append(row.id)
+
+	for row in __session.query(timehutDataSchema.Moment):
+		__momentIndexList.append(row.id)
+
+	return __collectionIndexList, __momentIndexList
+
+def updateDBCollection(data_list, existed_index_list, last_updated_time, session):
+	"""
+
+	:param data_list
+	:return: 
+	"""
+	if not isinstance(data_list, list):
+		# If it's an single object, then put it in the list to simplify the following logic
+		data_list = [data_list]
+
+	for data in data_list:
+		if isinstance(data, timehutDataSchema.Collection):
+			if data.id not in existed_index_list:
+				# Insert collection object
+				session.add(data)
+			elif data.updated_at > last_updated_time:
+				# Update collection object
+				session.query(timehutDataSchema.Collection)\
+						.filter(timehutDataSchema.Collection.id == data.id)\
+						.update({timehutDataSchema.Collection.updated_at: data.updated_at,
+								timehutDataSchema.Collection.caption: data.caption})
+		else: 
+			timehutLog.logging.error(f'[{sys._getframe().f_code.co_name}] Wrong Collection Type')
+			return False
+	# else:
+	# 	return purged_data_list
+
+def updateDBMoment(data_list, existed_index_list, last_updated_time, session):
+	"""
+
+	:param data_list
+	:return: 
+	"""
+	if not isinstance(data_list, list):
+		# If it's an single object, then put it in the list to simplify the following logic
+		data_list = [data_list]
+
+	for data in data_list:
+		if isinstance(data, timehutDataSchema.Moment):
+			if data.id not in existed_index_list:
+				# Insert collection object
+				session.add(data)
+			elif data.updated_at > last_updated_time:
+				# Update collection object
+				session.query(timehutDataSchema.Moment)\
+						.filter(timehutDataSchema.Moment.id == data.id)\
+						.update({timehutDataSchema.Moment.updated_at: data.updated_at,
+								timehutDataSchema.Moment.content: data.content})
+		else: 
+			timehutLog.logging.error(f'[{sys._getframe().f_code.co_name}] Wrong Moment Type')
+			return False
 
 # main function()
-def main(baby, days):
+def main(baby, days, last_updated_time):
 	try:
-		days = int(days)
+		__before_day = int(days)
 	except Exception as e:
 		__before_day = -200
 		# __before_day = 3000
-		logging.error(e)
-		# raise ValueError
+		timehutLog.logging.error(e)
 
-	__before_day = days
 
 	if baby != '1':
 		# Mui Mui Baby ID
@@ -216,22 +276,32 @@ def main(baby, days):
 		# On On Baby ID
 		__baby_id = 537413380
 
+
+	try:
+		last_updated_time = float(last_updated_time)
+	except Exception as e:
+		last_updated_time = 0.0
+
 	__dbName = "peekaboo"
 	__logging = False
 	__engine = createEngine(__dbName, timehutDataSchema.base, __logging)
+
+	collection_index_list, moment_index_list = generateIndexList(__engine)
+
 	DBSession = sessionmaker(bind=__engine)
 	__session = DBSession()
 
 	while (True):
+		# Getting original response body and next_index to decide what's the next request to submit
 		__next_index, __response_body = getCollectionRequest(__baby_id, __before_day)
-		collection_list = parseCollectionBody(__response_body)
 
-		__session.add_all(collection_list)
+		collection_list = parseCollectionBody(__response_body)
+		updateDBCollection(collection_list, collection_index_list, last_updated_time, __session)
 
 		for collection in collection_list:
 			__response_body = getMomentRequest(collection.id)
 			moment_list = parseMomentBody(__response_body)
-			__session.add_all(moment_list)
+			updateDBMoment(moment_list, moment_index_list, last_updated_time, __session)
 
 		__session.commit()
 
@@ -243,8 +313,10 @@ def main(baby, days):
 	__session.close()
 
 
-# check
+# Basic interactive interface
 if __name__ == "__main__":
 	baby = input(f'Do you want to get data for \n1) Anson or \n2) Angie\n')
 	days = input(f'What days you would like to start with: \n -200 (default) ~ XXXXX:\n')
-	main(baby, days)
+	#TODO: Should be improved by using a file to log the lasttime update time
+	last_updated_time = input(f'When did you run this script last time? (YYYY-MM-DDTHH:MM:SS.XXXZ)\n')
+	main(baby, days, last_updated_time)
---
+++ pythonTools/timehutLog.py
@@ -0,0 +1,8 @@
+import logging
+
+log_file_name = 'timehut.log'
+
+logging.basicConfig(filename=log_file_name,
+					format='%(asctime)s -%(name)s-%(levelname)s-%(module)s:%(message)s',
+					datefmt='%Y-%m-%d %H:%M:%S %p',
+					level=logging.DEBUG)
