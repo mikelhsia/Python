ec0f4e86f0fc43c8629734bb94938b79849630b1
mikelhsia <mikelhsia@hotmail.com>
2018-08-01 21:52:41 +0800

    [Python Tools - Timehut scrapy] Integrate existing mySQL database 1. Changing DATABASE setting in settings.py 2. Use python manage.py inspectdb > models.py to generate Mysql schema 3. Modify models.py schema 4. python manage.py migrate

--- PythonTools/myTimehutBlog/timehutBlog/models.py
+++ PythonTools/myTimehutBlog/timehutBlog/models.py
@@ -1,8 +1,14 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
 # we will use the reverse() method that allows you to build URLs by their name and passing optional parameters.
+from django.db.models import CharField
 from django.urls import reverse
 
 # Create your manager here.
@@ -11,75 +17,84 @@ class MomentManager(models.Manager):
 		return super(MomentManager, self).get_queryset().filter(content_type=3)
 
 # Create your models here.
-class Collection(models.Model):
+class PeekabooCollection(models.Model):
+
 	COLLECTION_TYPE =  (
 		(1, 'Collection'),
- 		(2, 'Text'),
- 		(3, 'Picture'),
- 		(4, 'Video'),
+		(2, 'Text'),
+		(3, 'Picture'),
+		(4, 'Video'),
 	)
 
 	# The convention in Django is to add a get_absolute_url() method to the model that returns the canonical URL of the object.
 	def get_absolute_url(self):
 		# reverse need to first have url pattern in urls.py named
-		return reverse('timehutBlog:collection_detail', args=[self.collection_id])
+		return reverse('timehutBlog:collection_detail', args=[self.id])
 
-	collection_id = models.CharField(max_length=32)
-	baby_id = models.CharField(max_length=32)
-	created_at = models.IntegerField()
-	updated_at = models.IntegerField()
-	months = models.IntegerField()
-	days = models.IntegerField()
-	content_type = models.PositiveSmallIntegerField()
-	# content_type = models.CharField(max_length=24, choice=COLLECTION_TYPE)
-	caption = models.TextField()
-	
-	# auto_now_add here, the date will be saved automatically when creating an object
-	# created = models.DateTimeField(auto_now_add=True)
+	# null 是针对数据库而言，如果
+	# null = True, 表示数据库的该字段可以为空。
+	# blank 是针对表单的
+	# blank = True, 表示你的表单填写该字段的时候可以不填
+	id = models.CharField(primary_key=True, max_length=32)
+	baby_id = models.CharField(max_length=32, blank=True, null=True)
+	created_at = models.IntegerField(blank=True, null=True)
+	updated_at = models.IntegerField(blank=True, null=True)
+	months = models.IntegerField(blank=True, null=True)
+	days = models.IntegerField(blank=True, null=True)
+	content_type = models.SmallIntegerField(blank=True, null=True)
+	caption = models.TextField(blank=True, null=True)
+	'''
+		# auto_now_add here, the date will be saved automatically when creating an object
+		created = models.DateTimeField(auto_now_add=True)
 
-	# auto_now here, the date will be updated automatically when saving an object.
-	# updated = models.DateTimeField(auto_now=True)
+		# auto_now here, the date will be updated automatically when saving an object.
+		updated = models.DateTimeField(auto_now=True)
 
-	# unique_for_date parameter to this field so we can build URLs
-	# for posts using the date and slug of the post. Django will prevent from
-	# multiple posts having the same slug for the same date
-	# slug = models.SlugField(max_length=250, unique_for_date='created_at')
-	slug = models.SlugField(max_length=250)
+		# unique_for_date parameter to this field so we can build URLs
+		# for posts using the date and slug of the post. Django will prevent from
+		# multiple posts having the same slug for the same date
+		slug = models.SlugField(max_length=250, unique_for_date='created_at')
+	'''
 
-
-	# The class Meta inside the model contains metadata. Telling Django to sort results by "created_at" field
 	class Meta:
+		# managed = False
+		# managed＝True则告诉django可以对数据库进行操作
+		managed = True
+		db_table = 'peekaboo_collection'
 		ordering = ('-created_at',)
 
-	def __str__(self):
-		return f"{self.baby_id} - {self.collection_id}"
+	# 修改admin后台的显示
+	# def __str__(self):
+	# 	return f"{self.baby_id} - {self.id}"
 
 
-class Moment(models.Model):
+class PeekabooMoment(models.Model):
 	MOMENT_TYPE =  (
 		(1, 'Text'),
- 		(2, 'Rich_text'),
- 		(3, 'Picture'),
- 		(4, 'Video'),
+		(2, 'Rich_text'),
+		(3, 'Picture'),
+		(4, 'Video'),
 	)
 
-	moment_id = models.CharField(max_length=32)
-	event_id = models.ForeignKey(Collection, related_name='event_id', on_delete='PROTECT')
-	baby_id = models.CharField(max_length=32)
-	created_at = models.IntegerField()
-	updated_at = models.IntegerField()
-	content_type = models.PositiveSmallIntegerField()
-	content = models.TextField()
-	src_url = models.CharField(max_length=512)
-	months = models.IntegerField()
-	days = models.IntegerField()
-	slug = models.SlugField(max_length=250)
+	id = models.CharField(primary_key=True, max_length=32)
+	event_id = models.ForeignKey(PeekabooCollection, on_delete=models.DO_NOTHING,
+	                             related_name='event_id', blank=True, null=True)
+	baby_id = models.CharField(max_length=32, blank=True, null=True)
+	created_at = models.IntegerField(blank=True, null=True)
+	updated_at = models.IntegerField(blank=True, null=True)
+	content_type = models.SmallIntegerField(blank=True, null=True)
+	content = models.TextField(blank=True, null=True)
+	src_url = models.CharField(max_length=512, blank=True, null=True)
+	months = models.IntegerField(blank=True, null=True)
+	days = models.IntegerField(blank=True, null=True)
 
 	class Meta:
+		managed = True
+		db_table = 'peekaboo_moment'
 		ordering = ('-created_at',)
 
-	def __str__(self):
-		return f"{self.baby_id} - {self.moment_id} - {self.event_id}"
+	# def __str__(self):
+	# 	return f"{self.baby_id} - {self.id} - {self.event_id}"
 
 	# The default manager
 	objects = models.Manager()
--- PythonTools/myTimehutBlog/timehutBlog/views.py
+++ PythonTools/myTimehutBlog/timehutBlog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Collection, Moment
+from .models import PeekabooCollection, PeekabooMoment
 
 # Create your views here.
 def collection_list(request):
-	collections = Collection.objects.all()
+	collections = PeekabooCollection.objects.all()
 	return render(request, 'collection/collection_list.html', {'collections': collections})
 
 def collection_detail(request, collection_id):
-	collection = get_object_or_404(Collection, collection_id=collection_id)
+	collection = get_object_or_404(PeekabooCollection, id=collection_id)
 	return render(request, 'collection/collection_detail.html', {'collection': collection})
