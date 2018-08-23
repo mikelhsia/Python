8982c51dea5bb11fee9b7616809ebabe99b7d84c
mikelhsia <mikelhsia@hotmail.com>
2018-08-23 23:41:40 +0800

    [Python Django - online store] First commit Todo: Attribute error in backend

---
+++ pythonDjango/onlineShop/onlineShop/shop/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Category, Product
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug']
+	prepopulated_fields = {'slug': ('name',)}
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+	list_filter = ['available', 'created', 'updated']
+	# Any field in list_editable must also be listed in the list_display attribute, since only the fields displayed can be edited.
+	list_editable = ['price', 'stock', 'available']
+	prepopulated_fields = {'stock': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
