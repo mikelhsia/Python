c1b8e83c94219ece532f7f2976c24113efad6db3
Michael <michael@MichaeldeMacBook-Pro.local>
2018-08-24 16:55:33 +0800

    [Python Django - online store]o 1. Fix list_view 2. Fix template c.name

--- pythonDjango/onlineShop/onlineShop/shop/views.py
+++ pythonDjango/onlineShop/onlineShop/shop/views.py
@@ -5,7 +5,7 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
 	category = None
 	categories = Category.objects.all()
-	products = Product.Objects.filter(available=True)
+	products = Product.objects.filter(available=True)
 
 	if category_slug:
 		category = get_object_or_404(Category, slug=category_slug)
@@ -14,7 +14,7 @@ def product_list(request, category_slug=None):
 	return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
 
 
-def product_detail(request, id, slug):
-	product = get_object_or_404(Product, id=id, slug=slug, available=True)
+def product_detail(request, id, category_slug):
+	product = get_object_or_404(Product, id=id, slug=category_slug, available=True)
 
 	return render(request, 'shop/product/detail.html', {'product': product})
