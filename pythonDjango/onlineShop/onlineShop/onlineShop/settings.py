2d05d1397e22c61b1a2c1a3d66938967d8746a04
mikelhsia <mikelhsia@hotmail.com>
2018-09-06 09:26:09 +0800

    [Python Django - online store] 1. Pipenv install celery and flower 2. task and celery config files to start asynchronous works

--- pythonDjango/onlineShop/onlineShop/onlineShop/settings.py
+++ pythonDjango/onlineShop/onlineShop/onlineShop/settings.py
@@ -130,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # This is the key that we're going to use to store the cart in the user session
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends/console.EmailBackend'
--- pythonDjango/onlineShop/onlineShop/orders/views.py
+++ pythonDjango/onlineShop/onlineShop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Order, OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .task import order_created
 
 # Create your views here.
 def order_create(request):
@@ -16,9 +17,15 @@ def order_create(request):
 
 			# Clear the cart
 			cart.clear()
+
+			# Launch asynchronous task
+			order_created.delay(order.id)
+
 			return render(request, 'orders/order/created.html', {'order': order})
 
 	else:
 		form = OrderCreateForm()
 
 	return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
