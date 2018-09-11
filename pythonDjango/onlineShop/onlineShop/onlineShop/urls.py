4ab00778326c10df072d0bd86f45e1b83ce0bb82
Michael <michael@MichaeldeMacBook-Pro.local>
2018-09-11 21:26:10 +0800

    [Python Django - online store] 1. Second commit for paypal payment

--- pythonDjango/onlineShop/onlineShop/onlineShop/urls.py
+++ pythonDjango/onlineShop/onlineShop/onlineShop/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('cart/', include('cart.urls', namespace='cart')),
 	path('orders/', include('orders.urls', namespace='orders')),
+        path('payment/', include('payment.urls', namespace='payment')),
 	path('', include('shop.urls', namespace='shop')),
 ]
