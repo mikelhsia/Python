0ecbc7b7d42e4f3f9e41b949482544403e8264fe
mikelhsia <mikelhsia@hotmail.com>
2018-10-13 08:53:01 +0800

    [Python Django - E-Learning System] 1. Abstract model implemented for content type

--- pythonDjango/educa/educa/courses/models.py
+++ pythonDjango/educa/educa/courses/models.py
@@ -6,44 +6,89 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class Subject(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200, unique=True)
 
-    class Meta:
-        ordering = ('title',)
+	class Meta:
+		ordering = ('title',)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
 
 class Course(models.Model):
-    owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.PROTECT)
-    subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.PROTECT)
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
-    overview = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.PROTECT)
+	subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.PROTECT)
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200, unique=True)
+	overview = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ('-created', )
+	class Meta:
+		ordering = ('-created', )
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
 
 class Module(models.Model):
-    course = models.ForeignKey(Course, related_name="modules", on_delete=models.PROTECT)
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    
-    def __str__(self):
-        return self.title
+	course = models.ForeignKey(Course, related_name="modules", on_delete=models.PROTECT)
+	title = models.CharField(max_length=200)
+	description = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.title
 
 
 class Content(models.Model):
-    module = models.ForeignKey(Module, related_name='contents', on_delete=models.PROTECT)
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    # A GenericForeignKey field to the related object by combining the two previous fields
-    item = GenericForeignKey('content_type', 'object_id')
+	module = models.ForeignKey(Module, related_name='contents', on_delete=models.PROTECT)
+	# content_type = models.ForeignKey(ContentType)
+	content_type = models.ForeignKey(ContentType,
+	                                 limit_choices_to={'model_in':('text', 'video', 'image', 'file')},
+	                                 on_delete=models.PROTECT)
+	object_id = models.PositiveIntegerField()
+	# A GenericForeignKey field to the related object by combining the two previous fields
+	item = GenericForeignKey('content_type', 'object_id')
 
+'''
+Abstract models: 
+An abstract model is a base class in which you define fields you want to include in all child models. 
+Django doesn't create any database table for abstract models. A database table is created for each 
+child model, including the fields inherited from the abstract class and the ones defined in the child model.
+
+Multi-table inheritance: 
+In multi-table inheritance, each model corresponds to a database table. Django creates a 
+OneToOneField field for the relationship in the child's model to its parent. 
+To use multi-table inheritance, you have to subclass an existing model. Django will create a
+database table for both the original model and the sub-model
+
+Proxy models: 
+Proxy models are used to change the behavior of a model, for example, including additional methods or 
+different meta options. Both models operate on the database table of the original model.
+'''
+# We're using abstract models here
+class ItemBase(models.Model):
+	# We need different related_name for each sub_model. So %(class)s is a placeholder for the model class name
+	owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.PROTECT)
+	title = models.CharField(max_length=250)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+	def __str__(self):
+		return self.title
+
+
+class Text(ItemBase):
+	content = models.TextField()
+
+class File(ItemBase):
+	file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+	image = models.ImageField(upload_to='images')
+
+class video(ItemBase):
+	url = models.URLField()
