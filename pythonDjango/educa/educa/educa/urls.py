39e1d572588d309d9d7616f99f48e639f6638e49
Michael <michael@MichaeldeMacBook-Pro.local>
2018-10-15 23:53:46 +0800

    [Python Django - E-Learning System] 1. overwrite login, loggout view

--- pythonDjango/educa/educa/educa/urls.py
+++ pythonDjango/educa/educa/educa/urls.py
@@ -10,12 +10,16 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include, 
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logged_user_out.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
