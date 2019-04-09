b946c8a24d5b8772a60cd9b519cfaa56a5132ffc
michael <mikelhsia@hotmail.com>
2019-04-09 18:35:12 +0800

    Working on spliting the DB management from getTimehut.py

---
+++ PythonTools/myTimehutScrapy/timehutManageDB.py
@@ -0,0 +1,129 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InternalError
+
+import timehutDataSchema
+import timehutLog
+
+DB_ACCOUNT="root"
+DB_PASSWORD="michael0512"
+DB_HOST="127.0.0.1"
+DB_PORT="3306"
+DB_ENCODING="utf-8"
+
+def createDB(dbName, base, loggingFlag):
+	engine = create_engine(f'mysql+pymysql://{DB_ACCOUNT}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}',
+	                       encoding=DB_ENCODING, echo=loggingFlag)
+
+	try:
+		engine.execute(f"CREATE DATABASE IF NOT EXISTS {dbName} DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+		timehutLog.logging.info(f"CREATE DATABASE IF NOT EXISTS {dbName} DEFAULT CHARSET utf8mb4 COLLATE utf8_general_ci;")
+
+		engine.execute(f"USE {dbName}")
+		timehutLog.logging.info(f"USE {dbName}")
+
+		ans = input(f"Do you want to drop the previous saved table (y/N)")
+
+		if ans == 'y' or ans == 'Y':
+			engine.execute(f"DROP TABLE {timehutDataSchema.Moment.__tablename__}")
+			timehutLog.logging.info(f"DROP TABLE {timehutDataSchema.Moment.__tablename__}")
+
+			engine.execute(f"DROP TABLE {timehutDataSchema.Collection.__tablename__}")
+			timehutLog.logging.info(f"DROP TABLE {timehutDataSchema.Collection.__tablename__}")
+
+	except InternalError as e:
+		base.metadata.create_all(engine)
+
+		engine.execute(f"ALTER DATABASE {dbName} CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;")
+		engine.execute(f"ALTER TABLE {timehutDataSchema.Collection.__tablename__} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
+		engine.execute(f"ALTER TABLE {timehutDataSchema.Collection.__tablename__} MODIFY caption TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+		engine.execute(f"ALTER TABLE {timehutDataSchema.Moment.__tablename__} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
+		engine.execute(f"ALTER TABLE {timehutDataSchema.Moment.__tablename__} MODIFY content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+
+
+
+	engine.dispose()
+
+
+def createEngine(dbName, base, loggingFlag):
+
+	createDB(dbName, base, loggingFlag)
+
+	engine = create_engine(f'mysql+pymysql://{DB_ACCOUNT}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{dbName}?charset=utf8mb4',
+	                       encoding=DB_ENCODING, echo=loggingFlag)
+
+	return engine
+
+
+def generateIndexList(engine):
+	DBSession = sessionmaker(bind=engine)
+	__session = DBSession()
+
+	__collectionIndexList = []
+	__momentIndexList = []
+
+	for row in __session.query(timehutDataSchema.Collection):
+		__collectionIndexList.append(row.id)
+
+	for row in __session.query(timehutDataSchema.Moment):
+		__momentIndexList.append(row.id)
+
+	__session.close()
+
+	return __collectionIndexList, __momentIndexList
+
+
+def updateDBCollection(data_list, existed_index_list, last_updated_time, session):
+	"""
+
+	:param data_list
+	:return: 
+	"""
+	if not isinstance(data_list, list):
+		# If it's an single object, then put it in the list to simplify the following logic
+		data_list = [data_list]
+
+	for data in data_list:
+		if isinstance(data, timehutDataSchema.Collection):
+			if data.id not in existed_index_list:
+				# Insert collection object
+				session.add(data)
+			elif data.updated_at > last_updated_time:
+				# Update collection object
+				session.query(timehutDataSchema.Collection)\
+						.filter(timehutDataSchema.Collection.id == data.id)\
+						.update({timehutDataSchema.Collection.updated_at: data.updated_at,
+								timehutDataSchema.Collection.caption: data.caption})
+		else:
+			timehutLog.logging.error(f'[{sys._getframe().f_code.co_name}] Wrong Collection Type')
+			return False
+	else:
+		session.commit()
+
+
+def updateDBMoment(data_list, existed_index_list, last_updated_time, session):
+	"""
+
+	:param data_list
+	:return: 
+	"""
+	if not isinstance(data_list, list):
+		# If it's an single object, then put it in the list to simplify the following logic
+		data_list = [data_list]
+
+	for data in data_list:
+		if isinstance(data, timehutDataSchema.Moment):
+			if data.id not in existed_index_list:
+				# Insert collection object
+				session.add(data)
+			elif data.updated_at > last_updated_time:
+				# Update collection object
+				session.query(timehutDataSchema.Moment)\
+						.filter(timehutDataSchema.Moment.id == data.id)\
+						.update({timehutDataSchema.Moment.updated_at: data.updated_at,
+								timehutDataSchema.Moment.content: data.content})
+		else:
+			timehutLog.logging.error(f'[{sys._getframe().f_code.co_name}] Wrong Moment Type')
+			return False
+	else:
+		session.commit()
