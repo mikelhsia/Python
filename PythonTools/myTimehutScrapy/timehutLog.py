323e81bdab1fdf11fa25741682b57908dadbbd52
michael <mikelhsia@hotmail.com>
2019-05-07 10:12:28 +0800

    Better logging practice

--- PythonTools/myTimehutScrapy/timehutLog.py
+++ PythonTools/myTimehutScrapy/timehutLog.py
@@ -1,12 +1,17 @@
 import logging
+import os
 
 log_file_name = 'timehut.log'
 
-# TODO better logging system
-# 1. sys.log 2. operation.log
-logging.basicConfig(filename=log_file_name,
-					format='%(asctime)s -%(name)s-%(levelname)s-%(module)s:%(message)s',
-					datefmt='%Y-%m-%d %H:%M:%S %p',
-					level=logging.INFO)
+if os.getenv("TIMEHUT_DEBUG") is not None:
+    logging.basicConfig(filename=log_file_name,
+                        format='%(asctime)s -%(name)s-%(levelname)s-%(module)s:%(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S %p',
+                        level=logging.DEBUG)
+else:
+    logging.basicConfig(filename=log_file_name,
+                        format='%(asctime)s -%(name)s-%(levelname)s-%(module)s:%(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S %p',
+                        level=logging.INFO)
 
 print(f"Module {__file__} is loaded...")
